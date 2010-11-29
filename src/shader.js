0f9155f2a82d25e04eb1635f65a39282cc793d8b
Corentin Wallez <corentin@wallez.net>
2010-11-29 11:33:07 +0100

    Trying to make the shader API more flexible
    * You can attach shaders to a program and link later.
    * Shaders must be initialised with their source
    * You cannot set attributes/uniforms when the program is not linked

--- src/shader.js
+++ src/shader.js
@@ -3,60 +3,80 @@
 //Represents a WebGL Shader program
 //The constructor takes two shaders to be attached immediately
 //TODO allow the Shaders to be attached later ?
-wok.ShaderProgram = function(vertex, fragment){
+wok.ShaderProgram = function(){
     
     //Create the WebGL Shader program and decorate it
     var program = this.gl.createProgram();
     wok.instance(program, this);
-    
-    //Attach the shaders and return nothing if it fails
-    this.gl.attachShader(program, vertex);
-    this.gl.attachShader(program, fragment);
-    this.gl.linkProgram(program);
-    
-    if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
-        this.gl.error("Unable to initialize the shader program: " + this.gl.getProgramInfoLog(program));
-        return null;
-    }
-    
-    
-    //If it successfully linked we can build the attribute info table
-    program.attributes = {};
-    
-    var attributeNumber = this.gl.getProgramParameter(program, this.gl.ACTIVE_ATTRIBUTES);
-    
-    for(var i=0; i<attributeNumber; i++){
-        var activeInfo = this.gl.getActiveAttrib(program, i);
-        program.attributes[activeInfo.name] = {
-            arraySize: activeInfo.size, //A size of 1 probably means it is not an array
-            type: activeInfo.type,
-            handle: this.gl.getAttribLocation(program, activeInfo.name)
-        }
-    }
-    
-    
-    //and the uniform info table
-    program.uniforms = {};
-    
-    var uniformNumber = this.gl.getProgramParameter(program, this.gl.ACTIVE_UNIFORMS);
-    
-    for(var i=0; i<uniformNumber; i++){
-        var activeInfo = this.gl.getActiveUniform(program, i);
-        program.uniforms[activeInfo.name] = {
-            arraySize: activeInfo.size, //A size of 1 probably means it is not an array
-            type: activeInfo.type,
-            handle: this.gl.getUniformLocation(program, activeInfo.name)
-        }
-    }
 
-    this.gl.info("Succesfully linked the shader program");
+    program.linked = false;    
+    program.attributes = {};
+    program.uniforms = {};
+
+    //Arguments are shaders to be attached directly
+    for(var i=0; i<arguments.length; i++){
+        program.attach(arguments[i]);
+    }
     
-    //That's all folks
-    return program;
+    //Suppose that if there are 2 shaders or more we can link
+    //Why does someone need to attach multiple fs or vs ?
+    if(this.arguments.length > 1)
+        return program.link();     //Returns null if the link fails
+
+    
+    return program;    
 }
 
-
 wok.ShaderProgram.prototype = {
+
+    //Attach shader to the program
+    attach: function(shader){
+        this.gl.attachShader(this, shader);
+    },
+
+    //Try to link the shader
+    link: function(){
+        this.gl.linkProgram(this);
+
+        if (!this.gl.getProgramParameter(this, this.gl.LINK_STATUS)) {
+            this.gl.error("Unable to initialize the shader program: " + this.gl.getProgramInfoLog(this));
+            return null;
+        }
+        this.computeVariables();
+
+        return this;
+    },
+
+    //Creates the table containing the shader's uniforms and attributes
+    computeVariables: function(){
+
+        //Get all the attributes in the attribute info table
+        var attributeNumber = this.gl.getProgramParameter(this, this.gl.ACTIVE_ATTRIBUTES);
+        
+        for(var i=0; i<attributeNumber; i++){
+            var activeInfo = this.gl.getActiveAttrib(this, i);
+            this.attributes[activeInfo.name] = {
+                arraySize: activeInfo.size, //A size of 1 probably means it is not an array
+                type: activeInfo.type,
+                handle: this.gl.getAttribLocation(this, activeInfo.name)
+            }
+        }
+        
+        
+        //and the uniform info table
+        var uniformNumber = this.gl.getProgramParameter(this, this.gl.ACTIVE_UNIFORMS);
+        
+        for(var i=0; i<uniformNumber; i++){
+            var activeInfo = this.gl.getActiveUniform(this, i);
+            this.uniforms[activeInfo.name] = {
+                arraySize: activeInfo.size, //A size of 1 probably means it is not an array
+                type: activeInfo.type,
+                handle: this.gl.getUniformLocation(this, activeInfo.name)
+            }
+        }
+    
+        return this;    
+    },
 
     //TODO Allow the program to check if it is in use so as to avoid some unnecessary binds
     //Select this shader for use in the following draw calls
@@ -79,6 +99,7 @@ wok.ShaderProgram.prototype = {
     },
 
     //Given a dict containing buffers, binds shader attributes to these buffers
+    //FIXME: What to do if it is not linked ?
     setAttributes: function(attributes){
         for(var attr in attributes){
             if( !(attr in this.attributes) ){
@@ -94,6 +115,7 @@ wok.ShaderProgram.prototype = {
     },
 
     //Given a dict containing values, binds shader uniforms to these values
+    //FIXME: What to do if it is not linked ?
     setUniforms: function(uniforms){
 
         for(var uni in uniforms){
@@ -169,6 +191,8 @@ wok.Shader = function(shaderSource, type){
     //Create the WebGL Shader and decorate it
     var shader = this.gl.createShader(type);
     wok.instance(shader, this);    
+  
+    this.type = type;
     
     //If the compilation fail return nothing
     this.gl.shaderSource(shader, shaderSource);
@@ -182,10 +206,6 @@ wok.Shader = function(shaderSource, type){
     this.gl.info("Succesfully compiled the shader");
     
     return shader;
-}
-
-wok.Shader.prototype.destroy = function(){
-    this.gl.deleteShader(this);
 }
 
 //Builds a Shader from the given script element. This function will guess the 
@@ -206,5 +226,23 @@ wok.Shader.fromElement = function(element){
     }else{
         this.gl.warn("Shader element type must be x-shader/x-[fragment|shader]");
         return null;  // Unknown shader type
+    }
+}
+
+wok.Shader.prototype = {
+    destroy: function(){
+        this.gl.deleteShader(this);
+    },
+    
+    getType: function(){
+        return this.type;
+    },
+
+    isFragment: function(){
+        return this.type == this.gl.FRAGMENT_SHADER;
+    },
+    
+    isVertex: function(){
+        return this.type == this.gl.VERTEX_SHADER;
     }
 };
